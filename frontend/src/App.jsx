dd57a03fbe8816e73935fbbae74c9fa61275e80b
Aurélie <aurelie.demure.ad@gmail.com>
2023-04-18 11:04:21 +0200

    Added router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 import "./App.css";
+import Teams from "./pages/Teams";
+import Library from "./pages/Library";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/library" element={<Library />} />
+      </Routes>
     </div>
   );
 }
