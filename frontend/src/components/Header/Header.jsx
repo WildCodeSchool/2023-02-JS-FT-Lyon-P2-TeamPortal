93e7b45e369df6e1c9563b54c27bec16d35ba0ca
Teodor ZHEKOV <TeodorZhekov@Outlook.fr>
2023-04-19 11:31:32 +0200

    Fixed spacing issues in Header component.

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import "./Header.css";
 export default function Header() {
   return (
     <header>
-      <h1>Team<span id="title">Portal</span></h1>
+      <h1>
+        Team<span id="title">Portal</span>
+      </h1>
     </header>
   );
-};
+}
