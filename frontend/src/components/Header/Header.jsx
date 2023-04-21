8429c6b0cbaf9576df877460df15388b9d92955a
Ropie1981 <pierre.saumont@gmail.com>
2023-04-21 09:50:44 +0200

    adding NewsCard S02US05

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header>
       <h1>
-        Team<span id="title">TeamPortal</span>
+        Team<span id="title">Portal</span>
       </h1>
     </header>
   );
