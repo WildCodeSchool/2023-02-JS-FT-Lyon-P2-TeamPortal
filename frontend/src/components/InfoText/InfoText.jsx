b43d79b16e62992dbf78b1df1943b5f5633fac35
Ropie1981 <pierre.saumont@gmail.com>
2023-04-25 20:38:57 +0200

    Adding InfoText Component into Header

---
+++ frontend/src/components/InfoText/InfoText.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./InfoText.css";
+
+function InfoText() {
+  return (
+    <div className="InfoText-container">
+      <div className="InfoText">
+        <p>
+          <span>Annonces de la direction :</span> Les mises à jour importantes,
+          les changements de personnel, les nouvelles initiatives ou les
+          décisions stratégiques de la direction sont des informations clés à
+          partager avec l'ensemble du personnel.
+          <span>Résultats financiers :</span> La santé financière de
+          l'entreprise, y compris les revenus, les bénéfices, les marges et
+          d'autres indicateurs clés de performance, peut être partagée pour
+          aider les employés à comprendre la performance globale de
+          l'entreprise. <span>Événements et activités internes :</span> Les
+          annonces sur les événements à venir, les activités de renforcement
+          d'équipe, les célébrations d'anniversaires d'entreprise, les fêtes de
+          fin d'année, les activités de bien-être ou les initiatives de
+          responsabilité sociale de l'entreprise peuvent être partagées pour
+          promouvoir la culture d'entreprise et renforcer l'engagement des
+          employés.
+          <span>Opportunités de développement professionnel :</span> Les
+          informations sur les programmes de formation, les possibilités de
+          perfectionnement, les ateliers ou les conférences peuvent être
+          diffusées pour encourager le développement professionnel des employés
+          et les aider à progresser dans leur carrière.
+          <span>Changements dans les politiques et les procédures :</span> Les
+          mises à jour des politiques internes, des procédures opérationnelles
+          standard (SOP), des processus d'approbation ou des protocoles de
+          sécurité peuvent être partagées pour assurer la conformité des
+          employés et maintenir un environnement de travail sûr et productif.
+          <span>Reconnaissance des employés :</span> Les réalisations
+          exceptionnelles des employés, les promotions, les distinctions ou les
+          mentions honorifiques peuvent être annoncées pour reconnaître et
+          célébrer les contributions positives des employés à l'entreprise.
+          <span>Nouveaux produits ou services :</span> Les informations sur les
+          lancements de nouveaux produits ou services, les mises à jour des
+          gammes de produits ou les améliorations de produits peuvent être
+          partagées pour informer les employés des derniers développements de
+          l'entreprise et les aider à mieux servir les clients.
+          <span>Actualités du secteur :</span> Les tendances du marché, les
+          nouvelles de l'industrie, les études de marché ou les informations sur
+          la concurrence peuvent être diffusées pour sensibiliser les employés à
+          l'environnement commercial externe et les aider à mieux comprendre le
+          contexte dans lequel l'entreprise évolue.
+          <span>Informations sur les avantages sociaux :</span> Les mises à jour
+          sur les avantages sociaux offerts aux employés, tels que les régimes
+          d'assurance maladie, les régimes de retraite, les congés payés, les
+          congés de maternité/paternité, peuvent être partagées pour informer
+          les employés sur les avantages offerts par l'entreprise et les aider à
+          en tirer pleinement parti. <span>Communications internes :</span> Les
+          mises à jour sur les nouvelles initiatives de communication interne,
+          les outils de collaboration, les changements dans les canaux de
+          communication ou les informations sur les enquêtes de satisfaction des
+          employés peuvent être partagées pour améliorer la communication
+          interne et favoriser un meilleur engagement des employés.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default InfoText;
