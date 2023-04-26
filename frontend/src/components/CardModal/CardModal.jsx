27e50b7e45eb29e29cf33a0e358eccd06adcd5df
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2023-04-26 23:34:43 +0200

    created the modal & the info div + added some style + fixing ESlint problems

---
+++ frontend/src/components/CardModal/CardModal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./CardModal.css";
+
+function CardModal({ member }) {
+  return (
+    <div className="modalContainer">
+      <img className="imgModal" src={member.image.url} alt={member.name} />
+      <div className="modalBox">
+        <h1 className="member-title">{member.name}</h1>
+        <p className="job-title">Développeur Front-End</p>
+        <p className="gender">{member.appearance.gender}</p>
+        <p className="appearance">
+          {member.appearance.height[1]} , {member.appearance.weight[1]}
+        </p>
+        <p className="occupation">Occupation : {member.work.occupation}</p>
+        <p className="base">Base : {member.work.base}</p>
+      </div>
+    </div>
+  );
+}
+
+CardModal.propTypes = {
+  member: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.shape({
+      url: PropTypes.string.isRequired,
+    }).isRequired,
+    appearance: PropTypes.shape({
+      gender: PropTypes.string.isRequired,
+      height: PropTypes.arrayOf(PropTypes.string).isRequired,
+      weight: PropTypes.arrayOf(PropTypes.string).isRequired,
+    }).isRequired,
+    work: PropTypes.shape({
+      occupation: PropTypes.string.isRequired,
+      base: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+export default CardModal;
--- frontend/src/components/teamList/TeamList.jsx
+++ frontend/src/components/teamList/TeamList.jsx
@@ -2,17 +2,38 @@ import React, { useState, useEffect } from "react";
 import "./TeamList.css";
 import axios from "axios";
 import TeamCard from "../teamCard/TeamCard";
+import TeamDevData from "../teamData/TeamDevData";
+import TeamDesignData from "../teamData/TeamDesignData";
+import TeamBusinessData from "../teamData/TeamBusinessData";
+import CardModal from "../CardModal/CardModal";
 
 const HERO_API_KEY = import.meta.env.VITE_HERO_API_KEY;
 
-function TeamList() {
+export default function TeamList() {
   const [teamData, setTeamData] = useState([]);
   const [selectedRadio, setSelectedRadio] = useState("Everyone");
   const radios = ["Everyone", "Team DEV", "Team Design", "Team Business"];
+  const [show, setShow] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [memberIndex, setMemberIndex] = useState(0);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
+
+  const handleToggle = () => {
+    setShow(!show);
+  };
 
   useEffect(() => {
     axios
-      .get(`https://www.superheroapi.com/api.php/${HERO_API_KEY}/search/p`)
+      .get(`https://www.superheroapi.com/api.php/${HERO_API_KEY}/search/w`)
       .then((response) => setTeamData(response.data.results))
       .catch((error) => {
         alert(error.message);
@@ -20,14 +41,42 @@ function TeamList() {
   }, []);
 
   const whatIsTeam = (member) => {
-    if (parseInt(member.powerstats.intelligence, 10) > 80) {
+    if (selectedRadio === "Everyone") {
+      return "Everyone";
+    }
+    if (parseInt(member.powerstats.intelligence, 10) > 70) {
       return "Team DEV";
     }
     if (parseInt(member.powerstats.strength, 10) > 30) {
       return "Team Design";
     }
-    return "Team Business";
+    if (parseInt(member.powerstats.speed, 10) > 20) {
+      return "Team Business";
+    }
+    return null;
   };
+
+  const whatIsTeamData = () => {
+    if (selectedRadio === "Team DEV") {
+      return <TeamDevData />;
+    }
+    if (selectedRadio === "Team Design") {
+      return <TeamDesignData />;
+    }
+    if (selectedRadio === "Team Business") {
+      return <TeamBusinessData />;
+    }
+    return null;
+  };
+
+  const getMemberByIndex = (index) => {
+    // console.log(`l'index est de ${index}`);
+    setMemberIndex(index);
+    setModal(true);
+  };
+
+  // console.log(`member index est égal à ${memberIndex}`);
+
   return (
     <div className="TeamList">
       <ul className="radio-container">
@@ -44,27 +93,50 @@ function TeamList() {
           </li>
         ))}
       </ul>
-      <div className="TeamMembers">
-        <ul>
-          {selectedRadio !== "Everyone"
-            ? teamData &&
-              teamData
-                .filter((members) => whatIsTeam(members) === selectedRadio)
-                .map((members) => (
-                  <li key={members.name}>
-                    <TeamCard members={members} />
-                  </li>
-                ))
-            : teamData &&
-              teamData.map((members) => (
-                <li key={members.name}>
-                  <TeamCard members={members} />
-                </li>
-              ))}
-        </ul>
+      {selectedRadio !== "Everyone" ? (
+        <button type="button" className="btn-toggle" onClick={handleToggle}>
+          Show details
+        </button>
+      ) : (
+        ""
+      )}
+      {show ? whatIsTeamData() : ""}
+
+      <div className={show ? "smallTeamMembers" : "greatTeamMembers"}>
+        {teamData &&
+          teamData
+            .filter((members) => whatIsTeam(members) === selectedRadio)
+            .map((members, index) => (
+              <button
+                className="member-button"
+                type="button"
+                key={members.id}
+                onClick={() => getMemberByIndex(index)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") getMemberByIndex(index);
+                }}
+              >
+                <TeamCard members={members} />
+              </button>
+            ))}
       </div>
+
+      {modal && (
+        <div className="modal">
+          <div className="overlay">
+            <div className="modal-content">
+              <CardModal member={teamData[memberIndex]} />
+              <button
+                type="button"
+                className="close-modal"
+                onClick={toggleModal}
+              >
+                CLOSE
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default TeamList;
