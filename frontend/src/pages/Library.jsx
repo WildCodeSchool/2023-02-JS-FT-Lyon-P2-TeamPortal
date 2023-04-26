202f1a87f961b8256254eb717cc77b89f9ce9280
Aurélie <aurelie.demure.ad@gmail.com>
2023-04-26 10:50:42 +0200

    removed comment

--- frontend/src/pages/Library.jsx
+++ frontend/src/pages/Library.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import LibraryList from "../components/LibraryList/LibraryList";
+
 import LibraryCard from "../components/LibraryCard/LibraryCard";
 
 const libraries = [
