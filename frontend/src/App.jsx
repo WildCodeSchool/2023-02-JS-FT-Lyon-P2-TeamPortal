10c7307e81799da6bcb986353cf13b277c6e4557
Aurélie <aurelie.demure.ad@gmail.com>
2023-05-03 16:16:38 +0200

    Removal of the loader from the news card and team pages and addition to the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo } from "react";
 import Home from "./pages/Home";
 import Teams from "./pages/Teams";
 import Library from "./pages/Library";
@@ -7,29 +7,12 @@ import Login from "./pages/Login";
 import UserContext from "./contexts/UserContext";
 
 import "./App.css";
-import Loader from "./components/Loader/Loader";
 
 function App() {
-  const [loader, setLoader] = useState(true);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoader(false);
-  //   }, 8000);
-  // }, []);
-
   const [user, setUser] = useState(null);
   const memoizedUser = useMemo(() => ({ user, setUser }), [user, setUser]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoader(false);
-    }, 15000);
-  }, []);
-
-  return loader ? (
-    <Loader />
-  ) : (
+  return (
     <div className="App">
       <UserContext.Provider value={memoizedUser}>
         <Routes>
