ce46cfee9991b0d8c6da768a6ed98e28401138a4
teodor-zhekov <teodorzhekov@outlook.fr>
2023-05-04 14:46:42 +0200

    Last major fix for the Weather Widget.

--- frontend/src/components/WeatherWidget/WeatherWidget.jsx
+++ frontend/src/components/WeatherWidget/WeatherWidget.jsx
@@ -5,7 +5,6 @@ import "./WeatherWidget.css";
 export default function WeatherWidget() {
   const url = `https://api.open-meteo.com/v1/dwd-icon?latitude=45.764043&longitude=4.835659&hourly=relativehumidity_2m&hourly=apparent_temperature,surface_pressure,windspeed_10m&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto&past_days=1`;
   const notify = () => toast.error("Sorry, no access to weather !");
-
   const weatherCodes = {
     0: "clear sky",
     1: "mainly clear",
@@ -35,8 +34,8 @@ export default function WeatherWidget() {
     95: "slight thunderstorm",
     96: "thunderstorm with hail",
   };
-  const [data, setData] = useState(null);
 
+  const [data, setData] = useState(null);
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
@@ -45,17 +44,45 @@ export default function WeatherWidget() {
       })
       .catch((error) => notify(error.message));
   }, []);
+
+  const currentDate = new Date();
+  const dayOfWeek = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ];
+  const currentDay = dayOfWeek[currentDate.getDay()];
+  const monthNames = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre",
+  ];
+  const currentMonth = monthNames[currentDate.getMonth()];
+  const currentYear = currentDate.getFullYear();
+  const formattedDate = `${currentDay}, ${currentDate.getDate()} ${currentMonth}, ${currentYear}`;
+
   return (
     <div className="weather-widget-container">
       {data ? (
         <>
           <div className="top">
             <div className="day-date-location">
-              <p className="day">Mardi</p>
-              <p className="date">26 avril 2023</p>
+              <p className="date">{formattedDate}</p>
               <p className="location">Lyon, FR</p>
             </div>
-
             <div className="weather-icon">
               <figcaption>
                 <img
@@ -69,46 +96,39 @@ export default function WeatherWidget() {
               </figcaption>
             </div>
           </div>
-
           <div className="bottom">
             <div className="degrees-container">
               <h1 className="degrees">
                 {Math.round(data.current_weather.temperature)}°C
               </h1>
-
               <p className="min-max-temperature">
                 <span className="min-temperature">
                   {Math.round(data.daily.temperature_2m_min[0])}°C
                 </span>{" "}
-                /{" "}
                 <span className="max-temperature">
                   {Math.round(data.daily.temperature_2m_max[0])}°C
                 </span>
               </p>
             </div>
-
             <div className="more-information">
               <div className="feels-like">
-                <p className="label">feels like:</p>
+                <p className="label">ressenti:</p>
                 <p className="content">
-                  {Math.round(data.current_weather.temperature) - 1}°C
+                  {Math.round(data.current_weather.temperature) - 3}°C
                 </p>
               </div>
-
               <div className="humidity">
-                <p className="label">humidity:</p>
+                <p className="label">humidité:</p>
                 <p className="content">{data.hourly.relativehumidity_2m[0]}%</p>
               </div>
-
               <div className="wind">
-                <p className="label">wind:</p>
+                <p className="label">vent:</p>
                 <p className="content">
                   {Math.round(data.current_weather.windspeed)} m/s
                 </p>
               </div>
-
               <div className="pressure">
-                <p className="label">pressure:</p>
+                <p className="label">pression:</p>
                 <p className="content">
                   {Math.round(data.hourly.surface_pressure[0])} hPa
                 </p>
